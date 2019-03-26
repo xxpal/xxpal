f0dda3d37a0bbdc60dc9cc30b188f0ae077e111e
Leo <xxpal@live.com>
2019-03-26 13:37:34 +0800

    updated

---
+++ Shushan/demo/py3/scratch.py
@@ -0,0 +1,22 @@
+# var = 6
+#
+#
+# def func():
+#     # var = 9
+#     global var
+#     print(var)
+#     # var = 9
+#
+#
+# func()
+# print(var)
+
+x = 'a                       '
+y = 'a                       '
+print(x is y)
+
+m = 1234567890
+n = 1234567890
+print(id(m))
+print(id(n))
+print(m is n)
